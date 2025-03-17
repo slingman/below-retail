6051945ca5993ae37251020e1a08660aa473dd95
Steve Ling <steveling@gmail.com>
2025-03-17 11:13:35 -0700

    Update selenium_setup.py

--- utils/selenium_setup.py
+++ utils/selenium_setup.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-def get_driver(headless=False):
+def get_chrome_driver():
+    """Returns a Selenium WebDriver instance with webdriver-manager handling chromedriver installation."""
     options = Options()
-    
-    if headless:
-        options.add_argument("--headless")  # Run in headless mode
-    
+    options.add_argument("--headless")  # Remove this if you want to see the browser
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--disable-blink-features=AutomationControlled")  # Helps avoid detection
     options.add_argument("--window-size=1920,1080")
+
+    service = Service(ChromeDriverManager().install())  # Automates Chromedriver setup
+    driver = webdriver.Chrome(service=service, options=options)
     
-    driver = webdriver.Chrome(options=options)
     return driver
