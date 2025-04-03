1c0bf0de2b468a44e2b79562d5f940880436a9fd
Steve Ling <steveling@gmail.com>
2025-04-02 21:34:31 -0700

    Update selenium_setup.py

--- utils/selenium_setup.py
+++ utils/selenium_setup.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-def create_webdriver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")  # Modern headless mode
+def create_webdriver(headless=True):
+    options = Options()
+
+    if headless:
+        options.add_argument("--headless=new")  # New headless mode for Chrome 109+
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
 
-    service = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    return driver
